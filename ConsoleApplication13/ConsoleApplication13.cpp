deaa9a533ba17aba764e78d1e301b60bac5f4d79
Егор <@DESKTOP-VF4U8Q2>
2024-10-24 17:38:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    int food = 100;
+    cout << "Всего гречки было на начало подсчёта: " << food << endl;
+
+    for (int month = 0; food > 0; month++) {
+        food -= 4;
+        cout << "После " << month << " месяца у вас в запасе останется " << food << " кг гречки." << endl;
+
+        if (food <= 0) {
+            cout << "После " << month << " месяца гречка закончится." << endl;
+            break;
+        }
+    }
+}
